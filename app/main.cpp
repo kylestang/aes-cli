69acb59eed0d2bdac9ebe28fd361f8ec8b45d0ed
Hal <haln_01@proton.me>
2024-08-06 18:33:50 -0700

    fix: key parsing

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,18 @@
+#include <format>
 #include <io/io.hpp>
 
 int main(int arg, char* argv[]) {
-    const io::IO io{io::parse_cli(arg, argv)};
+    io::IO io{io::parse_cli(arg, argv)};
+    std::cout << "lkasdflkjsd" << std::endl;
+
+    crypto::Block buf{};
+    std::size_t bytes_read = 0;
+    while ((bytes_read = io.read(buf)) == crypto::BLOCK_SIZE) {
+        std::cout << std::format("read {} bytes", bytes_read) << std::endl;
+    }
+
+    // last block
+    std::cout << std::format("read {} bytes", bytes_read) << std::endl;
 
     return 0;
 }
