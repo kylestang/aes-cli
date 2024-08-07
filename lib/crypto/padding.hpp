097b976174ed723d933fc40b29f920375f922e86
Hal <haln_01@proton.me>
2024-08-07 15:20:52 -0700

    fix: passing `padding::pad_` tests

--- lib/crypto/padding.hpp
+++ lib/crypto/padding.hpp
@@ -3,7 +3,7 @@
 
 namespace padding {
 
-inline void pad_(crypto::Block& buf, std::size_t full_block_size,
+inline void pad_(uint8_t* buf, std::size_t full_block_size,
                  std::size_t pad_size) {
     for (std::size_t i = 0; i < pad_size; ++i) {
         buf[full_block_size - 1 - i] = pad_size;
@@ -12,7 +12,7 @@ inline void pad_(crypto::Block& buf, std::size_t full_block_size,
 
 // add padding for a 128 bit block `buf`
 inline void pad_block(crypto::Block& buf) {
-    pad_(buf, crypto::BLOCK_SIZE, crypto::BLOCK_SIZE - buf.size());
+    pad_(buf.begin(), crypto::BLOCK_SIZE, crypto::BLOCK_SIZE - buf.size());
 }
 
 }  // namespace padding
