b0431a4a01423379534c2aae86c376af052c714d
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2024-08-08 13:37:02 -0700

    fix: move files into subfolders

--- lib/tables.hpp
+++
@@ -1,7 +0,0 @@
-
-#include <array>
-#include <cstdint>
-
-extern std::array<std::array<uint8_t, 16>, 16> s_box;
-extern std::array<uint8_t, 256> multiply_by_2;
-extern std::array<uint8_t, 256> multiply_by_3;
