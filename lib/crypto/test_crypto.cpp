c20238a1065291a351f5da606cc06c8752cfe6be
Hal <haln_01@proton.me>
2024-08-09 11:18:37 -0700

    fix: using `Buffer` struct

--- lib/crypto/test_crypto.cpp
+++ lib/crypto/test_crypto.cpp
@@ -1,5 +1,3 @@
-#include <algorithm>
-#include <array>
 #include <catch2/catch_test_macros.hpp>
 #include <crypto/crypto.hpp>
 #include <cstddef>
@@ -22,9 +20,6 @@ TEST_CASE("crypto::Buffer::pad_pkcs7") {
     {
         Buffer buf{{}, 0};
         buf.pad_pkcs7();
-
-        Block expected{};
-        std::fill(expected.begin(), expected.end(), BLOCK_SIZE);
         for (const uint8_t& b : buf.block()) {
             REQUIRE(b == BLOCK_SIZE);
         }
@@ -40,7 +35,7 @@ bool operator==(const Buffer& left, const Buffer& right) {
     return true;
 }
 
-TEST_CASE("crypto::Block - xor") {
+TEST_CASE("crypto::Buffer - xor") {
     const Buffer a{{11, 13, 10, 5, 9, 12, 13, 15, 8, 4, 9, 6, 3, 1, 3, 6},
                    BLOCK_SIZE};
     const Buffer b{{14, 5, 8, 8, 2, 5, 1, 15, 12, 0, 11, 12, 0, 0, 5, 9},
