6e6e96bdfde23866fc468e9fdd47916a4925da55
Hal <haln_01@proton.me>
2024-08-09 11:18:37 -0700

    fix: using `Buffer` struct

--- lib/crypto/ciphermode.cpp
+++ lib/crypto/ciphermode.cpp
@@ -5,92 +5,100 @@
 namespace crypto::ciphermode {
 
 // CipherMode abstract class
-CipherMode::CipherMode(AES& key, Block iv) : key_{key}, diffusion_block_{iv} {};
+CipherMode::CipherMode(AES& key, Buffer iv)
+    : key_{key}, diffusion_block_{iv} {};
 
-void CipherMode::key_encrypt_inplace(Block& block) noexcept {
+void CipherMode::key_encrypt_inplace(Buffer& block) noexcept {
     // TODO: for Kyle
 }
 
-void CipherMode::key_decrypt_inplace(Block& block) noexcept {
+void CipherMode::key_decrypt_inplace(Buffer& block) noexcept {
     // TODO: for Kyle
 }
 
 // ECB
-ECB::ECB(AES& key) : CipherMode{key, Block{}} {}
+ECB::ECB(AES& key) : CipherMode{key, Buffer{}} {}
 
-void ECB::encrypt_inplace(Block& block) noexcept { key_encrypt_inplace(block); }
+void ECB::encrypt_inplace(Buffer& block) noexcept {
+    key_encrypt_inplace(block);
+}
 
-void ECB::decrypt_inplace(Block& block) noexcept { key_decrypt_inplace(block); }
+void ECB::decrypt_inplace(Buffer& block) noexcept {
+    key_decrypt_inplace(block);
+}
 
 // CBC
-CBC::CBC(AES& key, Block iv) : CipherMode{key, iv} {}
+CBC::CBC(AES& key, Buffer iv) : CipherMode{key, iv} {}
 
-void CBC::encrypt_inplace(Block& plaintext) noexcept {
+void CBC::encrypt_inplace(Buffer& plaintext) noexcept {
     plaintext ^= diffusion_block_;
     key_encrypt_inplace(plaintext);
     diffusion_block_ = plaintext;  // plaintext is now the ciphertext
 }
 
-void CBC::decrypt_inplace(Block& ciphertext) noexcept {
-    const Block new_diff_block = ciphertext;
+void CBC::decrypt_inplace(Buffer& ciphertext) noexcept {
+    const Buffer new_diff_block = ciphertext;
     key_decrypt_inplace(ciphertext);
     ciphertext ^= diffusion_block_;
     diffusion_block_ = new_diff_block;
 }
 
 // GCM
-GCM::GCM(AES& key, Block iv)
-    : CipherMode{key, iv}, counter_0_{encrypt_cp(iv)}, H_{encrypt_cp(Block{})} {
+GCM::GCM(AES& key, Buffer iv)
+    : CipherMode{key, iv},
+      counter_0_{encrypt_cp(iv)},
+      H_{encrypt_cp(Buffer{})} {
     key_encrypt_inplace(tag_);
 
     // the actual message starts with counter value 1
     gcm_utils::inc_counter(diffusion_block_);
 }
 
-void GCM::encrypt_general(Block& m) noexcept {
-    Block ctr_register{diffusion_block_};
+void GCM::encrypt_general(Buffer& m) noexcept {
+    Buffer ctr_register{diffusion_block_};
     key_encrypt_inplace(ctr_register);
     m ^= ctr_register;
     gcm_utils::inc_counter(diffusion_block_);
 };
 
-void GCM::encrypt_inplace(Block& plaintext) noexcept {
+void GCM::encrypt_inplace(Buffer& plaintext) noexcept {
     encrypt_general(plaintext);
 
     // TODO: compute tag
     payload_len_ += plaintext.size();
 }
 
-void GCM::decrypt_inplace(Block& ciphertext) noexcept {
+void GCM::decrypt_inplace(Buffer& ciphertext) noexcept {
     encrypt_general(ciphertext);
 
     // TODO: compute tag
     payload_len_ += ciphertext.size();
 }
 
-Block GCM::encrypt_cp(const Block& block) noexcept {
-    Block buf{block};
+Buffer GCM::encrypt_cp(const Buffer& block) noexcept {
+    Buffer buf{block};
     encrypt_inplace(buf);
     return buf;
 };
 
 namespace gcm_utils {
 
-void inc_counter(Block& block) noexcept {
+void inc_counter(Buffer& buffer) noexcept {
+    Block& block{buffer.block()};
     for (uint8_t i = BLOCK_SIZE - 1; i >= gcm_utils::IV_SIZE; --i) {
         ++block[i];
         if (block[i] != 0) return;
     }
 }
 
-Block make_gcm_iv() noexcept {
+Buffer make_gcm_iv() noexcept {
     std::random_device dev;
     std::mt19937 rng{dev()};
     std::uniform_int_distribution<std::mt19937::result_type> dist{0, 0xff};
 
-    Block iv{};
+    Buffer iv{};
     for (uint8_t i = 0; i < gcm_utils::IV_SIZE; ++i) {
-        iv[i] = dist(rng);
+        iv.block()[i] = dist(rng);
     }
 
     return iv;
--- lib/crypto/ciphermode.hpp
+++ lib/crypto/ciphermode.hpp
@@ -1,4 +1,4 @@
-#include <crypto/crypto.cpp>
+#include <crypto/crypto.hpp>
 
 namespace crypto::ciphermode {
 
@@ -10,21 +10,21 @@ class CipherMode {
 
     protected:
         AES& key_;  // Kyle update this key to your AES class
-        Block diffusion_block_;
+        Buffer diffusion_block_;
 
         // Kyle: go to ciphermode.cpp and implement these
-        void key_encrypt_inplace(Block& block) noexcept;
-        void key_decrypt_inplace(Block& block) noexcept;
+        void key_encrypt_inplace(Buffer& block) noexcept;
+        void key_decrypt_inplace(Buffer& block) noexcept;
 
     public:
-        CipherMode(AES& key, Block iv);
+        CipherMode(AES& key, Buffer iv);
         ~CipherMode() = default;
 
         // encrypt the *padded* block in place
-        virtual void encrypt_inplace(Block& plaintext) noexcept = 0;
+        virtual void encrypt_inplace(Buffer& plaintext) noexcept = 0;
 
         // decrypt the *padded* block in place
-        virtual void decrypt_inplace(Block& ciphertext) noexcept = 0;
+        virtual void decrypt_inplace(Buffer& ciphertext) noexcept = 0;
 
         // don't need these
         CipherMode() = delete;
@@ -37,41 +37,41 @@ class CipherMode {
 class ECB : CipherMode {
     public:
         ECB(AES&);
-        void encrypt_inplace(Block& plaintext) noexcept override;
-        void decrypt_inplace(Block& ciphertext) noexcept override;
+        void encrypt_inplace(Buffer& plaintext) noexcept override;
+        void decrypt_inplace(Buffer& ciphertext) noexcept override;
 };
 
 class CBC : CipherMode {
     private:
-        Block diffusion_block_;
+        Buffer diffusion_block_;
 
     public:
-        CBC(AES& key, Block iv);
-        void encrypt_inplace(Block& plaintext) noexcept override;
-        void decrypt_inplace(Block& ciphertext) noexcept override;
+        CBC(AES& key, Buffer iv);
+        void encrypt_inplace(Buffer& plaintext) noexcept override;
+        void decrypt_inplace(Buffer& ciphertext) noexcept override;
 };
 
 class GCM : CipherMode {
     private:
-        Block diffusion_block_;
-        Block tag_{};
-        const Block counter_0_;
+        Buffer diffusion_block_;
+        Buffer tag_{};
+        const Buffer counter_0_;
         std::size_t payload_len_{0};
-        const Block H_;
+        const Buffer H_;
 
     public:
-        GCM(AES& key, Block iv);
-        void encrypt_inplace(Block& plaintext) noexcept override;
-        void decrypt_inplace(Block& ciphertext) noexcept override;
+        GCM(AES& key, Buffer iv);
+        void encrypt_inplace(Buffer& plaintext) noexcept override;
+        void decrypt_inplace(Buffer& ciphertext) noexcept override;
 
     private:
         // Since the encryption/decryption of payload is the
         // same, only the auth tag is slightly different...
-        void encrypt_general(Block& block) noexcept;
+        void encrypt_general(Buffer& block) noexcept;
 
         // To encrypt counter 0 for auth tag, and to
         // initialize `H` multiplication variable
-        Block encrypt_cp(const Block& block) noexcept;
+        Buffer encrypt_cp(const Buffer& block) noexcept;
 };
 
 namespace gcm_utils {
@@ -80,11 +80,11 @@ constexpr std::size_t IV_SIZE = 12;
 
 // increment the counter bytes (last 4 bytes)
 // of `block`
-void inc_counter(Block&) noexcept;
+void inc_counter(Buffer&) noexcept;
 
 // make `IV`, with 12 random bytes and
 // and 4 bytes counter initialized to 0
-Block make_gcm_iv() noexcept;
+Buffer make_gcm_iv() noexcept;
 
 }  // namespace gcm_utils
 
