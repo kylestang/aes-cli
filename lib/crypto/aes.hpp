d6d40803130be72df9b7a4cce290306ec28c860b
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2024-08-11 18:34:16 -0700

    fix: recoveralmostlostfilesthatwasclose

---
+++ lib/crypto/aes.hpp
@@ -0,0 +1,11 @@
+#include <crypto/crypto.hpp>
+#include <crypto/key.hpp>
+
+namespace crypto {
+Block sub_bytes(const Block matrix);
+Block encrypt(Block block, AesKey key);
+Block shift_rows(const Block matrix);
+Block mix_columns(const Block matrix);
+Block add_round_key(Block block, AesKey aes_key, size_t round);
+Block encrypt(Block block, AesKey key);
+}  // namespace crypto
